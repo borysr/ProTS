33f38d3cb7cff2f8f21d4eb92c7c29676ea4a16b
Borys Romanyuk <Borys.Romanyuk@inmar.com>
2017-09-07 23:25:16 -0500

    generic classes

---
+++ generic_classes.ts
@@ -0,0 +1,18 @@
+// tslint:disable typedef
+class DomainId<T> {
+    constructor(public id: T) {
+    }
+    get value(): T {
+        return this.id;
+    }
+}
+class OrderId extends DomainId<number> {
+    constructor(public orderIdValue: number) {
+        super(orderIdValue);
+    }
+}
+class AccountId extends DomainId<string> {
+    constructor(public accountIdValue: string) {
+        super(accountIdValue);
+    }
+}
